b7ad5e10a892cdd8f58eb2b42eac58ee527d9e69
bribironke <br.ibironke@gmail.com>
2023-09-26 17:20:01 +0100

    updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { createRequire } from 'module'
+import path from 'path'
+
+const { resolve } = createRequire(import.meta.url)
+
+const prismaClient = `prisma${path.sep}client`
+
+const prismaClientIndexBrowser = resolve('@prisma/client/index-browser').replace(`@${prismaClient}`, `.${prismaClient}`)
+
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+	},
+	resolve: { alias: { '.prisma/client/index-browser': path.relative(__dirname, prismaClientIndexBrowser) } },
 });
