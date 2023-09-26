4e21723870e0a613b2a69d05633771bf39e17248
bribironke <br.ibironke@gmail.com>
2023-09-26 22:25:04 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,8 @@
   <title>Dhub</title>
 </svelte:head>
 
-<div class="row flex flex-col gap-2 flex-center">
-  <h1 aria-label="welcome">Welcome 🚀 to Dhub Gallery</h1>
-  <p>This web application allows users to upload images and submit their contact information. The application provides a user-friendly interface and follows best practices for web development.</p>
+<div class="row flex flex-col gap-4 flex-center">
+  <h1 aria-label="welcome" class="text-2xl">Welcome 🚀 to Dhub Gallery</h1>
+  <p>This web application allows users to upload images (.jpg, .png, .gif formats) and submit their contact information. Register and login to upload your picture.</p>
+  <a href="/upload" class="bt bt-main w-fit">upload now!</a>
 </div>
