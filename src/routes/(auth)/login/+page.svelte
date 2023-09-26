47d9da4992b976abf2ec00c9b6b772328c6215dc
bribironke <br.ibironke@gmail.com>
2023-09-26 16:43:54 +0100

    updated auth pages

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -6,16 +6,16 @@
 
 
 <div class="w-full h-full flex justify-center items-center">
-  <form action="?/login" method="post" class="mx-auto flex flex-col gap-4 dcard w-[300px]">
+  <form action="?/login" method="post" class="mx-auto flex flex-col gap-4 dcard w-[300px] md:w-[480px]">
     <h1 class="text-2xl font-semibold">Login</h1>
     <hr class="dark:opacity-40"/>
     <div class="flex flex-col w-full gap-1">
       <label for="email">Email</label>
-      <input type="email" name="email" placeholder="E.g. john.doe@gmail.com" id="email" class="input input-bordered w-full max-w-xs bg-transparent border-font-color/40 dark:border-white/40" />
+      <input type="email" name="email" placeholder="E.g. john.doe@gmail.com" id="email" class="input input-bordered w-full bg-transparent border-font-color/40 dark:border-white/40" />
     </div>
     <div class="flex flex-col w-full gap-1">
       <label for="password">Password</label>
-      <input type="password" name="password" id="password" class="input input-bordered w-full max-w-xs bg-transparent border-font-color/40 dark:border-white/40" />
+      <input type="password" name="password" id="password" class="input input-bordered w-full bg-transparent border-font-color/40 dark:border-white/40" />
     </div>
     {#if form?.invalid}
       <p class="text-red-500 dark:text-white text-sm font-semibold">*Email and password are required.</p>
@@ -25,6 +25,6 @@
       <p class="text-red-500 dark:text-white text-sm font-semibold">*You have entered the wrong credentials</p>
     {/if}
 
-    <button type="submit" class="btn btn-primary">Login</button>
+    <button type="submit" class="bt bt-main outline-none">Login</button>
   </form>
 </div>
--- src/routes/(auth)/register/+page.svelte
+++ src/routes/(auth)/register/+page.svelte
@@ -4,31 +4,35 @@
   export let form: ActionData
 </script>
 
-<h1>Register</h1>
-<form action="?/register" method="post">
-  <div>
-    <label for="username">Username</label>
-    <input type="text" name="username" id="username" placeholder="E.g. John Doe" required />
-  </div>
-  <div>
-    <label for="email">Email</label>
-    <input type="email" name="email" id="email" placeholder="E.g. john.doe@gmail.com" required />
-  </div>
-  <div>
-    <label for="phoneNumber">Phone Number</label>
-    <input type="text" name="phoneNumber" id="phoneNumber" placeholder="E.g. +1 832 310 8276" required />
-  </div>
-  <div>
-    <label for="address">Address</label>
-    <textarea name="address" id="address" placeholder="E.g. 5514 Griggs Rd. Houston TX, 77021 USA" rows="5"></textarea>
-  </div>
-  <div>
-    <label for="password">Password</label>
-    <input type="password" name="password" id="password" required />
-  </div>
+<div class="w-full h-full flex justify-center items-center">
 
-  {#if form?.user}
-    <p class="error">Username is taken</p>
-  {/if}
-  <button type="submit">Register</button>
-</form>
+  <form action="?/register" method="post" class="mx-auto flex flex-col gap-4 dcard w-[300px] md:w-[480px]">
+    <h1 class="text-2xl font-semibold">Register</h1>
+    <hr class="dark:opacity-40"/>
+    <div class="flex flex-col w-full gap-1">
+      <label for="username">Username</label>
+      <input type="text" name="username" id="username" placeholder="E.g. John Doe" class="input input-bordered w-full bg-transparent border-font-color/40 dark:border-white/40" />
+    </div>
+    <div class="flex flex-col w-full gap-1">
+      <label for="email">Email</label>
+      <input type="email" name="email" placeholder="E.g. john.doe@gmail.com" id="email" class="input input-bordered w-full bg-transparent border-font-color/40 dark:border-white/40" />
+    </div>
+    <div class="flex flex-col w-full gap-1">
+      <label for="phoneNumber">Phone Number</label>
+      <input type="text" name="phoneNumber" id="phoneNumber" placeholder="E.g. +1 832 310 8276" class="input input-bordered w-full bg-transparent border-font-color/40 dark:border-white/40" />
+    </div>
+    <div>
+      <label for="address" class="flex flex-col w-full gap-1">Address</label>
+      <input type="text" name="address" id="address" placeholder="E.g. 5514 Griggs Rd. Houston TX, 77021" class="input input-bordered w-full bg-transparent border-font-color/40 dark:border-white/40" />
+    </div>
+    <div class="flex flex-col w-full gap-1">
+      <label for="password">Password</label>
+      <input type="password" name="password" id="password" class="input input-bordered w-full bg-transparent border-font-color/40 dark:border-white/40" />
+    </div>
+  
+    {#if form?.user}
+      <p class="error">Username or email is taken</p>
+    {/if}
+    <button type="submit" class="bt bt-main outline-none">Register</button>
+  </form>
+</div>
